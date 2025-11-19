7a2e1231206908c2f6fec50860503aa0aef6dda3
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-11-19 03:21:03 -0300

    atualizado datetime

--- Services/OrdemDeCompraService.cs
+++ Services/OrdemDeCompraService.cs
@@ -69,7 +69,7 @@ namespace BackendDesapegaJa.Services
 
             ordem.valor_total = (int)total;
 
-            ordem.created_at = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+            ordem.created_at = DateTime.UtcNow;
 
             _repo.Adicionar(ordem);
 
